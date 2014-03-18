88c7549a5e49a0e4e2770ceba3a7292a740abd9d
heavycoder <heavycoder1@gmail.com>
2014-03-18 00:30:08 -0400

    Infinity Scroller

---
+++ Assets/Scripts/AddScore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if(collider.tag == "Player") {
+			Score.AddScore();
+		}
+	}
+}
